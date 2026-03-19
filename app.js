9b9e7fb15e4c28674691d65415a9c05c7d0ddbd3
smarthomesven <sven@dypodex.com>
2026-03-19 07:58:06 +0100

    Added telemetry

--- app.js
+++ app.js
@@ -98,6 +98,24 @@ module.exports = class OllamaApp extends Homey.App {
         throw new Error('Error generating response from Ollama: ' + error.message);
       }
     });
+    try {
+      const { randomUUID } = require('crypto');
+      let id = this.homey.settings.get('id');
+      if (!id) {
+        id = randomUUID();
+        this.homey.settings.set('id', id);
+      }
+      await axios.post('https://homey-apps-telemetry.vercel.app/api/installations', {
+        id: id,
+        appId: "nl.dypodex.proxy",
+        homeyPlatform: this.homey.platformVersion ? this.homey.platformVersion : 1,
+        appVersion: this.manifest.version,
+      }).catch(error => {
+        this.error('Error sending telemetry data:', error.message);
+      });
+    } catch (error) {
+      this.error('Error in onInit:', error.message);
+    }
   }
   async getImageBase64(image) {
     const stream = await image.getStream();
